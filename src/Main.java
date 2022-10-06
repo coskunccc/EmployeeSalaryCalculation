93046ebcac9ac1972fbe7e9a4a54a85158cda02f
coskunccc <coskunarslan@yahoo.com>
2022-10-06 19:27:20 +0300

    commit for ebebek

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee employee=new Employee("Mehmet Varan",2500,45,2005);
+        System.out.println(employee.toString());
+
+    }
+}
